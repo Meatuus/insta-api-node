24a65af7ce5c1028a4e15d7cad9612782c251731
Tyler <tcameronsmith@gmail.com>
2017-11-22 22:56:50 +1100

    add test to grab all my recent posts

--- authService.js
+++ authService.js
@@ -19,19 +19,32 @@ module.exports = function (req, res) {
     httpRequest(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var r = JSON.parse(body);
-            var user = {
-                id: r.user.id,
-                username: r.user.username,
-                full_name: r.user.full_name,
-                bio: r.user.bio,
-                website: r.user.website,
-                profile_picture: r.user.profile_picture,
-                access_token: r.access_token
+            // var user = {
+            //     id: r.user.id,
+            //     username: r.user.username,
+            //     full_name: r.user.full_name,
+            //     bio: r.user.bio,
+            //     website: r.user.website,
+            //     profile_picture: r.user.profile_picture,
+            //     access_token: r.access_token
+            // };
+            // User.create(user, function (error) {
+            //     if (error) res.send(error);
+            //     res.redirect('/');
+            // })
+            console.log(r);
+            var options = {
+                url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=' + r.access_token,
+                method: 'GET'
             };
-            User.create(user, function (error) {
-                if (error) res.send(error);
-                res.redirect('/');
-            })
+            httpRequest(options, function (error, response, body) {
+                if (error && response.statusCode != 200) {
+                    console.error(error);
+                } else {
+                    var jsonobjArr = JSON.parse(body);
+                    console.log(jsonobjArr);
+                }
+            });
         }
     });
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var axios = require('axios');
 
 var config = require('./config');
 
@@ -17,6 +18,24 @@ app.get('/', function (request, response) {
 var authUser = require('./authService');
 
 app.get('/auth', authUser);
+
+// app.get('/test', function (req, res) {
+//     axios.get('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=183919697.cdb951d.9f17f9a46a424ed7b45f4b06f4d9b295')
+//         .then(function (response) {
+//             console.log(response);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+//     // fetch('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=183919697.cdb951d.9f17f9a46a424ed7b45f4b06f4d9b295')
+//     // .then(function (response) {
+//     //     return response.blob();
+//     // }).then(function (myBlob) {
+//     //     // var objectURL = URL.createObjectURL(myBlob);
+//     //     // myImage.src = objectURL;
+//     //     console.log(myBlob);
+//     // });
+// })
 
 app.get('/login', function (request, response) {
     response.redirect(config.instagram.auth_url);
