bced77de6eee50f138c26497b8afa480ae73ab24
Tyler <tcameronsmith@gmail.com>
2017-11-23 21:14:54 +1100

    remove comments

--- server.js
+++ server.js
@@ -19,24 +19,6 @@ var authUser = require('./authService');
 
 app.get('/auth', authUser);
 
-// app.get('/test', function (req, res) {
-//     axios.get('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=183919697.cdb951d.9f17f9a46a424ed7b45f4b06f4d9b295')
-//         .then(function (response) {
-//             console.log(response);
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         });
-//     // fetch('https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=183919697.cdb951d.9f17f9a46a424ed7b45f4b06f4d9b295')
-//     // .then(function (response) {
-//     //     return response.blob();
-//     // }).then(function (myBlob) {
-//     //     // var objectURL = URL.createObjectURL(myBlob);
-//     //     // myImage.src = objectURL;
-//     //     console.log(myBlob);
-//     // });
-// })
-
 app.get('/login', function (request, response) {
     response.redirect(config.instagram.auth_url);
 });
